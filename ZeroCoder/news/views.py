f64ba7dc434273f009be249b493514744f0cdccc
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-27 21:38:18 +0300

    Nina's project

--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,3 +1,10 @@
+
 from django.shortcuts import render
+from .models import News_post
+
+def home(request):
+	news = News_post.objects.all()
+	return render(request, 'news/news.html', {'news': news})
+
 
 # Create your views here.
