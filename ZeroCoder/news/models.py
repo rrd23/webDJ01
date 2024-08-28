f791852f12bf9a68a2e864210548e2a6cdd3dc1e
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-28 09:04:26 +0300

    homework DJ03

--- ZeroCoder/news/models.py
+++ ZeroCoder/news/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class News_post(models.Model):
-	title = models.CharField('Название новости', max_length=50)
-	short_description = models.CharField('Краткое описание новости', max_length=200)
-	text = models.TextField('Новость')
-	pub_date = models.DateTimeField('Дата публикации')
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+    user_name = models.CharField('Имя пользователя', max_length=100)  # Новое поле
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-
-	class Meta:
-		verbose_name = 'Новость'
-		verbose_name_plural = 'Новости'
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
