8fc87354acce457ed7e295805ec0716d923febd6
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-29 15:17:15 +0300

    zip

--- ZeroCoder/news/forms.py
+++ ZeroCoder/news/forms.py
@@ -1,7 +1,13 @@
 from .models import News_post
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea, DateInput
 
 class News_postForm(ModelForm):
-	class Meta:
-		model = News_post
-		fields = ['title', 'short_description', 'text', 'pub_date']
+    class Meta:
+        model = News_post
+        fields = ['title', 'short_description', 'text', 'pub_date',]
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок'}),
+            'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание'}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание'}),
+            'pub_date': DateInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'})
+        }
