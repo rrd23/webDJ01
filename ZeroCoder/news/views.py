ea89967cb91bf58bc1da181f4ae2e321796027f5
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-29 14:35:38 +0300

    add mew form
    form.py

--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,10 +1,12 @@
 
 from django.shortcuts import render
 from .models import News_post
+from .forms import News_postForm
 
 def home(request):
 	news = News_post.objects.all()
 	return render(request, 'news/news.html', {'news': news})
 
 def create_news(request):
-	return render(request, 'news/add_new_post.html')
+	form = News_postForm()
+	return render(request, 'news/add_new_post.html', {'form': form})
